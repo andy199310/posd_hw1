55e80637e6072067a3ba171a78364b371e9d41f3
Green <andy199310@gmail.com>
2016-10-18 12:25:31 +0800

    modified .travis.yml for #3

---
+++ travis.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+./posd_hw1 | tee output.txt
+echo "Program Ended!"
+tail output.txt
+count=$(grep 'There were .* failures' output.txt | wc -l)
+if [ "$count" -ne "0" ];
+  then
+    echo "Error!"
+    exit -1;
+fi
+echo "Success!"
+exit 0
