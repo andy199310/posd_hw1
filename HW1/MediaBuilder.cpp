65ce1456436b6f75a262c343ba4b7b144a91bc00
Green <andy199310@gmail.com>
2016-11-01 18:13:53 +0800

    remove unwant code in MediaBuilder

--- HW1/MediaBuilder.cpp
+++ HW1/MediaBuilder.cpp
@@ -33,32 +33,20 @@ void MediaBuilder::buildShapeMedia() {
 }
 
 void MediaBuilder::buildRectangle(double x, double y, double width, double height) {
-//    Rectangle* rectangle = new Rectangle(x, y, width, height);
-//    ShapeMedia *shapeMedia = new ShapeMedia(rectangle);
-//    baseMediaStack.push(shapeMedia);
-//    Media *shapeMedia = dynamic_cast<Media*>(baseMediaStack.top());
     baseMediaStack.top()->add(new ShapeMedia(new Rectangle(x, y, width, height)));
 }
 
 void MediaBuilder::buildCircle(double centerX, double centerY, double radius) {
-//    Circle *circle = new Circle(centerX, centerY, radius);
-//    ShapeMedia *shapeMedia = new ShapeMedia(circle);
-//    baseMediaStack.push(shapeMedia);
     baseMediaStack.top()->add(new ShapeMedia(new Circle(centerX, centerY, radius)));
-//    ShapeMedia *shapeMedia = dynamic_cast<ShapeMedia*>(baseMediaStack.top());
-//    shapeMedia->add(new Circle(centerX, centerY, radius));
 }
 
 void MediaBuilder::buildTriangle(double point1X, double point1Y, double point2X, double point2Y, double point3X, double point3Y) {
-//    ShapeMedia *shapeMedia = dynamic_cast<ShapeMedia*>(baseMediaStack.top());
-//    baseMediaStack.top()->add(new ShapeMedia(new Circle(centerX, centerY, radius)));
     baseMediaStack.top()->add(new ShapeMedia(new Triangle(point1X, point1Y, point2X, point2Y, point3X, point3Y)));
-//    shapeMedia->add(new Triangle(point1X, point1Y, point2X, point2Y, point3X, point3Y));
 }
 
 Media *MediaBuilder::getBaseShape() {
-//    if(baseMediaStack.size() != 1){
-//        throw "There is error in building level";
-//    }
+    if(baseMediaStack.size() != 1){
+        throw "There is error in building level";
+    }
     return baseMediaStack.top();
 }
