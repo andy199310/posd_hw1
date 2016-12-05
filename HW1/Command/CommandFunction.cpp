1dc1aae1a71c3e36a2075310f4da8703fb1b8139
Green <andy199310@gmail.com>
2016-12-05 12:05:21 +0800

    refactor spilt function

--- HW1/Command/CommandFunction.cpp
+++ HW1/Command/CommandFunction.cpp
@@ -8,20 +8,12 @@
 #include <math.h>
 
 unsigned int CommandFunction::split(const std::string &txt, std::vector<std::string> &strs, char ch) {
-    unsigned int pos = txt.find( ch );
-    unsigned int initialPos = 0;
-    strs.clear();
+    std::stringstream stream(txt);
+    std::string tmpString;
 
-    // Decompose statement
-    while( pos != std::string::npos ) {
-        strs.push_back( txt.substr( initialPos, pos - initialPos + 1 ) );
-        initialPos = pos + 1;
-
-        pos = txt.find( ch, initialPos );
+    while(std::getline(stream, tmpString, ch)){
+        strs.push_back(tmpString);
     }
-
-    // Add the last one
-    strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
 
     return strs.size();
 }
--- HW1/Command/DefCommand.cpp
+++ HW1/Command/DefCommand.cpp
@@ -47,7 +47,7 @@ void DefCommand::execute(std::string command) {
             compositeMedia->add(media);
         }
 
-        std::string key = argumentList.at(1).substr(0, argumentList.at(1).length()-1);
+        std::string key = argumentList.at(1).substr(0, argumentList.at(1).length());
         _application->addMedia(key, compositeMedia);
 
         return;
@@ -55,7 +55,7 @@ void DefCommand::execute(std::string command) {
         if(CommandFunction::startWith(argumentList.at(3), "Circle")){
             std::vector<double> itemVector;
             CommandFunction::getShapeArgumentFromString(&itemVector, argumentList.at(3));
-            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length()-1);
+            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length());
             _application->addMedia(key, new ShapeMedia(new Circle(itemVector.at(0), itemVector.at(1), itemVector.at(2))));
 
             return;
@@ -63,7 +63,7 @@ void DefCommand::execute(std::string command) {
         if(CommandFunction::startWith(argumentList.at(3), "Rectangle")){
             std::vector<double> itemVector;
             CommandFunction::getShapeArgumentFromString(&itemVector, argumentList.at(3));
-            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length()-1);
+            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length());
             _application->addMedia(key, new ShapeMedia(new Rectangle(itemVector.at(0), itemVector.at(1), itemVector.at(2), itemVector.at(3))));
 
             return;
@@ -71,7 +71,7 @@ void DefCommand::execute(std::string command) {
         if(CommandFunction::startWith(argumentList.at(3), "Triangle")){
             std::vector<double> itemVector;
             CommandFunction::getShapeArgumentFromString(&itemVector, argumentList.at(3));
-            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length()-1);
+            std::string key = argumentList.at(1).substr(0, argumentList.at(1).length());
             _application->addMedia(key, new ShapeMedia(new Triangle(itemVector.at(0), itemVector.at(1), itemVector.at(2), itemVector.at(3), itemVector.at(4), itemVector.at(5))));
 
             return;
