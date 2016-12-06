d12aeeb791791936291e2e0818d019b61811603d
Green <andy199310@gmail.com>
2016-12-06 16:06:01 +0800

    add shell contoller

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,9 +1,17 @@
 #include "../cppunitlite/TestHarness.h"
 #include "unitTest/utMain.h"
 
+#define OPEN_SHELL 0
+
 int main(){
     TestResult tr;
     TestRegistry::runAllTests(tr);
+
+    if(OPEN_SHELL){
+        Application application;
+        Shell shell(&application);
+        shell.start();
+    }
 
     return 0;
 }
